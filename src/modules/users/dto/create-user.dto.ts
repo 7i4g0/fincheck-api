818349614d24b7b9f373b62be35b45a115a248c4
Tiago Brito <gtiago.brito@hotmail.com>
2024-09-07 15:08:29 -0300

    feat: Implements PrismaORM and UserRepository

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+}
