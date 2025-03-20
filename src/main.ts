5d46b8db33d5a57ff74839e7f8a1afcdb19f9b78
Tiago Brito <gtiago.brito@hotmail.com>
2025-03-20 17:00:45 -0300

    Adiciona logs para deploy

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,23 @@ import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*',
-  });
+  try {
+    console.log('Iniciando aplicação...');
+    const app = await NestFactory.create(AppModule);
+    console.log('App criado com sucesso');
 
-  await app.listen(process.env.PORT || 3001);
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors({
+      origin: '*',
+    });
+
+    console.log('Configurações aplicadas');
+    await app.listen(process.env.PORT || 3001);
+    console.log('Aplicação rodando na porta:', process.env.PORT || 3001);
+  } catch (error) {
+    console.error('Erro ao iniciar aplicação:', error);
+    throw error;
+  }
 }
 
 if (process.env.NODE_ENV !== 'production') {
